7bb3dd133905c96301f35d746b1204858d73634c
gabriellapowell <soccerplayer60@icloud.com>
2018-06-12 17:54:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+  "Hello Gabriella."
+end 
+
+get '/hometown' do
+  "My hometown is Broomfield"
 end
+
+get  '/favorite-song'do
+"My favorite song is I have no clue"
+end 
+
+end 
